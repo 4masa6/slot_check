67d6c441afc1ecfc3466963e56def20668625feb
mogata <ms46.dev@gmail.com>
2021-12-01 09:31:23 +0900

    名前付きスロット確認

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
-    <HelloWorld title="slot">
-      <!-- HelloWorldコンポーネントの<slot />の部分にこのテキストが入る -->
+    <!-- <HelloWorld title="slot">
+      HelloWorldコンポーネントの<slot />の部分にこのテキストが入る
       <p>this is default text</p>
+    </HelloWorld> -->
+
+    <!-- 名前付きスロット -->
+    <HelloWorld>
+      <p>Begin</p>
+      <template v-slot:first>
+        First message
+      </template>
+      <p>Middle</p>
+      <template v-slot:second>
+        Second message
+      </template>
+      <p>End</p>
+      <template v-slot:third>
+        Third message
+      </template>
     </HelloWorld>
   </div>
 </template>
